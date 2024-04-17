8d308a4569eaa037fa5541145cb3f35c52503c42
Lexi Zhang <xizhang0708@gmail.com>
2024-04-17 15:01:18 -0400

    finished testing add()

--- tests/RangeList.test.ts
+++ tests/RangeList.test.ts
@@ -1,43 +1,71 @@
 import {IntensityManager} from "../src/RangeList";
 
 
-describe('IntensityManager', () => {
+describe('IntensityManager Operations', () => {
     let manager: IntensityManager;
 
     beforeEach(() => {
         manager = new IntensityManager();
     });
 
-    test('Start: [], Call: add(10, 30, 1) => [[10,1],[30,0]]', () => {
-        manager.update(10, 30, 1, 'add');
-        expect(manager.intensities).toEqual([[10, 1], [30, 0]]);
+    describe('add operation', () => {
+        test('Start: [], Call: update(10, 30, 1, \'add\') => [[10,1],[30,0]]', () => {
+            manager.update(10, 30, 1, 'add');
+            expect(manager.intensities).toEqual([[10, 1], [30, 0]]);
+        });
+
+        test('Start: [[10,1],[30,0]], update(20, 40, 1, \'add\') => [[10,1],[20,2],[30,1],[40,0]]', () => {
+            manager.update(10, 30, 1, 'add');
+            manager.update(20, 40, 1, 'add');
+            expect(manager.intensities).toEqual([[10, 1], [20, 2], [30, 1], [40, 0]]);
+        });
+
+        test('Start: [[10,1],[20,2],[30,1],[40,0]], Call: update(10, 40, -2, \'add\') => [[10,-1],[20,0],[30,-1],[40,0]]', () => {
+            manager.update(10, 30, 1, 'add');
+            manager.update(20, 40, 1, 'add');
+            manager.update(10, 40, -2, 'add');
+            expect(manager.intensities).toEqual([[10, -1], [20, 0], [30, -1], [40, 0]]);
+        });
+
+        test('Start: [[10,1],[20,2],[30,1],[40,0]], Call: update(10, 40, -1, \'add\') => [[20,1],[30,0]', () => {
+            manager.update(10, 30, 1, 'add');
+            manager.update(20, 40, 1, 'add');
+            manager.update(10, 40, -1, 'add');
+            expect(manager.intensities).toEqual([[20, 1], [30, 0]]);
+        });
+
+        test('Start: [[20, 1], [30, 0]], Call: update(10, 40, -1, \'add\') => [[10,-1],[20,0],[30,-1],[40,0]]', () => {
+            manager.update(10, 30, 1, 'add');
+            manager.update(20, 40, 1, 'add');
+            manager.update(10, 40, -1, 'add');
+            manager.update(10, 40, -1, 'add');
+            expect(manager.intensities).toEqual([[10, -1], [20, 0], [30, -1], [40, 0]]);
+        });
+
+        test('should remove consecutive duplicates', () => {
+            manager.update(10, 30, 1, 'add');
+            manager.update(20, 30, 1, 'add');
+            manager.update(20, 30, -1, 'add');
+            manager.update(40, 50, 0, 'add');
+            expect(manager.intensities).toEqual([[10, 1], [30, 0]]);
+        });
+
+        test('should handle no operation correctly', () => {
+            manager.update(10, 10, 0, 'add');
+            expect(manager.intensities).toEqual([]);
+        });
+
+        test('should trim leading and trailing zeros', () => {
+            manager.update(10, 20, 0, 'add');
+            manager.update(30, 40, 1, 'add');
+            manager.update(50, 60, 0, 'add');
+            manager.update(60, 70, 0, 'add');
+            expect(manager.intensities).toEqual([[30, 1], [40, 0]]);
+        });
     });
 
-    test('Start: [[10,1],[30,0]], Call: add(20, 40, 1) => [[10,1],[20,2],[30,1],[40,0]]', () => {
-        manager.update(10, 30, 1, 'add');
-        manager.update(20, 40, 1, 'add');
-        expect(manager.intensities).toEqual([[10, 1], [20, 2], [30, 1], [40, 0]]);
-    });
+    describe('set operation', () => {
 
-    test('Start: [[10,1],[20,2],[30,1],[40,0]], Call: add(10, 40, -2) => [[10,-1],[20,0],[30,-1],[40,0]]', () => {
-        manager.update(10, 30, 1, 'add');
-        manager.update(20, 40, 1, 'add');
-        manager.update(10, 40, -2, 'add');
-        expect(manager.intensities).toEqual([[10, -1], [20, 0], [30, -1], [40, 0]]);
-    });
-
-    test('Start: [], Sequential operations to mimic multiple updates and final state', () => {
-        manager.update(10, 30, 1, 'add');
-        expect(manager.intensities).toEqual([[10, 1], [30, 0]]);
-
-        manager.update(20, 40, 1, 'add');
-        expect(manager.intensities).toEqual([[10, 1], [20, 2], [30, 1], [40, 0]]);
-
-        manager.update(10, 40, -1, 'add');
-        expect(manager.intensities).toEqual([[20, 1], [30, 0]]);
-
-        manager.update(10, 40, -1, 'add');
-        expect(manager.intensities).toEqual([[10, -1], [20, 0], [30, -1], [40, 0]]);
     });
 
     afterEach(() => {
